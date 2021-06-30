1d53c25f2910cbd60099f18e2e73d37be85b5837
Ducnhu2k1 <ducnhu2k1@gmail.com>
2021-06-30 12:09:18 +0700

    module succcess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  {
+    path: '', component: HomePageComponent,
+    children: [
+      {
+        path: 'home-page',
+        component: HomePageComponent
+      }
+    ]
+
+  },
+  {
+    path: '**',
+    component: HomePageComponent
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
